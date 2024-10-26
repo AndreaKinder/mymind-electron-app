48786fba27f6ff3cce845d94492905b51093be8d
Andrea <144063021+AndreaKinder@users.noreply.github.com>
2024-10-26 23:25:23 +0200

    add pack base

---
+++ PKGBUILD
@@ -0,0 +1,20 @@
+pkgname=mymind-electron-app
+pkgver=1.0.0
+pkgrel=1
+pkgdesc="MyMind Electron App"
+arch=('x86_64')
+url="https://github.com/andreavillar/mymind-electron-app"
+license=('MIT')
+depends=('electron' 'nodejs')
+source=("https://github.com/andreavillar/mymind-electron-app/archive/v1.0.0.tar.gz")
+sha256sums=('YOUR_SHA256SUM')
+
+build() {
+  cd "$srcdir/mymind-electron-app-1.0.0"
+  makepkg -s
+}
+
+package() {
+  cd "$srcdir/mymind-electron-app-1.0.0/dist"
+  cp -r mymind-electron-app-* "$pkgdir/usr"
+}
---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    show: false
+  });
+
+  win.loadURL('https://access.mymind.com/everything');
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
